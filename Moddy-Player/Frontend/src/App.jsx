8e07d6c1199bf613c3b74114ee6eb4b69e1039d9
Rokusuke69 <chauhan.abhishek.motilal@gmail.com>
2025-10-25 23:50:09 +0530

    Changed the Frontend layout and started backend

--- Moddy-Player/Frontend/src/App.jsx
+++ Moddy-Player/Frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import FacialExpression from './components/FacialExpression'
+import MoodSongs from './components/MoodSongs'
 
 function App() {
 
   return (
     <>
      <FacialExpression/>
+     <MoodSongs/>
     </>
   )
 }
